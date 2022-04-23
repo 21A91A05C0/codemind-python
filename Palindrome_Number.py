d3f908c87f28e587a65262dd7d56ccd203a15037
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-04-23 22:19:43 +0530

    t=int(input()) while(t>0):     n=int(input())     t=n     rev=0     d=0     while(n>0):         d=n%10         rev=rev*10+d         n=n//10     if(rev==t):         print("True")     else:         print("False")         rev=0     t=t-1

---
+++ Palindrome_Number.py
@@ -0,0 +1,16 @@
+t=int(input())
+while(t>0):
+    n=int(input())
+    t=n
+    rev=0
+    d=0
+    while(n>0):
+        d=n%10
+        rev=rev*10+d
+        n=n//10
+    if(rev==t):
+        print("True")
+    else:
+        print("False")
+        rev=0
+    t=t-1
