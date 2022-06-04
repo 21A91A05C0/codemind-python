6382399fde6e51ac7f0c978bbc286e316081981a
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-06-04 22:40:51 +0530

    n=list(map(str,input().split())) l="aeiouAEIOU" k=[] c=0 for i in n:     c=0     for j in range(len(i)):         if i[j] in l:             c+=1     k.append(c) print(max(k))

---
+++ maximum_vowel_count.py
@@ -0,0 +1,12 @@
+n=list(map(str,input().split()))
+l="aeiouAEIOU"
+k=[]
+c=0
+for i in n:
+    c=0
+    for j in range(len(i)):
+        if i[j] in l:
+            c+=1
+    k.append(c)
+print(max(k))
+        
