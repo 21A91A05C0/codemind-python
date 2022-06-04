45752368b40f9ef5e32c4ee1a2e5577855db9fa9
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-06-04 11:06:18 +0530

    n=input() k="aeiouAEIOU" p=[]
    for i in range(len(n)):
        if n[i] in k:
            if  n[i] not in p:
                p.append(n[i])
    if(len(p)==0):
        print("-1")
    print(*(p))

---
+++ vowels_in__string.py
@@ -0,0 +1,13 @@
+n=input()
+k="aeiouAEIOU"
+p=[]
+
+for i in range(len(n)):
+    if n[i] in k:
+        if  n[i] not in p:
+            p.append(n[i])
+if(len(p)==0):
+    print("-1")
+print(*(p))
+        
+    
