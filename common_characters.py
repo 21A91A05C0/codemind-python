24a28ce9df50c8a178cd360a5b45e169d44ea601
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-07-16 23:21:39 +0530

    s1=input() s2=input() a1="" b1="" arr=[] for i in s1:     if i.isspace():         continue     else:         a1=a1+i.lower() for i in s2:     if i.isspace():         continue     else:         b1=b1+i.lower() for i in a1:     if i in b1:         if i not in arr:             arr.append(i) arr.sort() if(len(arr)==0):     print(-1) else:     for i in arr:         print(i,end="")

---
+++ common_characters.py
@@ -0,0 +1,25 @@
+s1=input()
+s2=input()
+a1=""
+b1=""
+arr=[]
+for i in s1:
+    if i.isspace():
+        continue
+    else:
+        a1=a1+i.lower()
+for i in s2:
+    if i.isspace():
+        continue
+    else:
+        b1=b1+i.lower()
+for i in a1:
+    if i in b1:
+        if i not in arr:
+            arr.append(i)
+arr.sort()
+if(len(arr)==0):
+    print(-1)
+else:
+    for i in arr:
+        print(i,end="")
