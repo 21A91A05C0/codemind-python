85f032ffba1acef09c9202b110ed73dc61473794
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-08-25 18:15:13 +0530

    n=int(input()) k=list(map(int,input().split())) c=0 d=0 for i in range(len(k)-1,1,-1):     if(k[i]==k[i-1]+k[i-2]):         c+=1     else:         d=1         break if(c!=0 and d==0):     print("yes") else:     print("no")

--- Fibonacci_array.py
+++ Fibonacci_array.py
@@ -1,14 +1,14 @@
 n=int(input())
-l=list(map(int,input().split()))
+k=list(map(int,input().split()))
 c=0
 d=0
-for i in range(len(l)-1,1,-1):
-    if(l[i]==l[i-1]+l[i-2]):
-        c=1
+for i in range(len(k)-1,1,-1):
+    if(k[i]==k[i-1]+k[i-2]):
+        c+=1
     else:
-        d=d+1
+        d=1
+        break
 if(c!=0 and d==0):
     print("yes")
 else:
     print("no")
-        
