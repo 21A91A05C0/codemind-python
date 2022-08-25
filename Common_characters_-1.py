0aad5e675b219348b69fe060bb8e7fffb770a2e0
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-08-25 15:05:47 +0530

    n=input().lower().split(" ") c=0 d=0 p=n[0] k="" for i in p:     c=0     for j in range(1,len(n)):         if i in n[j]:             c+=1
        if(c==len(n)-1):
            d+=1
            k=k+i
    if(len(k)!=0):
        print(k)
    else:
        print("-1")

--- Common_characters_-1.py
+++ Common_characters_-1.py
@@ -1,14 +1,16 @@
-n=input().lower().split()
+n=input().lower().split(" ")
+c=0
+d=0
 p=n[0]
 k=""
-d=0
 for i in p:
     c=0
     for j in range(1,len(n)):
         if i in n[j]:
-            c=c+1
+            c+=1
+            
     if(c==len(n)-1):
-        d=d+1
+        d+=1
         k=k+i
 if(len(k)!=0):
     print(k)
