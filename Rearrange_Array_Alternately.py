de3d8608e6405269e1fecca2aa5e8b4802fb49e7
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-08-28 13:53:23 +0530

    t=int(input()) for i in range(t):     n=int(input())     arr=list(map(int,input().split()))     l=[]     arr=sorted(arr)     i=-1     j=0     c=0     while(c<len(arr)):         if arr[i] not in l:             l.append(arr[i])         if arr[j] not in l:             l.append(arr[j])         i=i-1         j=j+1         c=c+2     print(*l)

---
+++ Rearrange_Array_Alternately.py
@@ -0,0 +1,20 @@
+t=int(input())
+for i in range(t):
+    n=int(input())
+    arr=list(map(int,input().split()))
+    l=[]
+    arr=sorted(arr)
+    i=-1
+    j=0
+    c=0
+    while(c<len(arr)):
+        if arr[i] not in l:
+            l.append(arr[i])
+        if arr[j] not in l:
+            l.append(arr[j])
+        i=i-1
+        j=j+1
+        c=c+2
+    print(*l)
+        
+        
