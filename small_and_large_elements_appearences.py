54ffbf22874ce74072b22a0f75dd673751d4659c
21A91A05C0 <97424374+21A91A05C0@users.noreply.github.com>
2022-09-15 23:12:00 +0530

    s=input() s1='' for i in s:     if(i==' '):         continue     else:         s1=s1+i print(min(s1),s1.count(min(s1)),max(s1),s1.count(max(s1)))

--- small_and_large_elements_appearences.py
+++ small_and_large_elements_appearences.py
@@ -1,16 +1,8 @@
-x = list(map(str,input().split()))
-b=[]
-c=[]
-for i in x:
-    n = max(i)
-    b.append(n)
-    m = min(i)
-    b.append(m)
-    for j in i:
-        c.append(j)
-a = min(b)
-d = max(b)
-print(a,end=" ")
-print(c.count(a),end=" ")
-print(d,end=" ")
-print(c.count(d),end=" ")
+s=input()
+s1=''
+for i in s:
+    if(i==' '):
+        continue
+    else:
+        s1=s1+i
+print(min(s1),s1.count(min(s1)),max(s1),s1.count(max(s1)))
